f0f6cac6531e2115b2c73192e126e0d261a3432c
James Wortley <jameswortley@James-Wortleys-MacBook-Pro.local>
2016-06-07 18:20:12 -0700

    added @media and mobile icon

--- javascript/data.js
+++ javascript/data.js
@@ -1,19 +1,30 @@
 //raw data obtained from somewehere
 var searchResult = [{
-  library: 'UNDERSCORE JS',
-  libraryURL:'http://www.underscorejs.org/',
-  category: 'utilities',
-  dateTested: '2016-3-1',
+  article: 'UNDERSCORE JS',
+  articleURL:'http://www.underscorejs.org/',
+  category: 'Software',
+  blogDate: '2016-3-1',
   review: '<p>this handly library allowed me to return values needing to write my for loops! Maybe next time for faster performance, I can try to search by dictionaries</p>'
-},{library: 'IMPRESS JS',
-  libraryURL:'http://impress.github.io/impress.js/',
-  category: 'presentation',
-  dateTested: '2016-4-1',
+},{article: 'IMPRESS JS',
+  articleURL:'http://impress.github.io/impress.js/',
+  category: 'Software',
+  blogDate: '2016-4-1',
   review: '<p>Great library for presentations! try not to be too creative with the flips and rotations, otherwise it will make your guests dizzy!</p>'
-},{library: 'D3 JS',
-  libraryURL:'https://d3js.org/',
-  category: 'presentation',
-  dateTested: '2016-4-15',
+},{article: 'D3 JS',
+  articleURL:'https://d3js.org/',
+  category: 'Software',
+  blogDate: '2016-4-15',
   review: '<p>Great library for data driven presentations!</p>'
+},{article: 'Paris',
+  articleURL:'http://http://en.parisinfo.com//',
+  category: 'Travel',
+  blogDate: '2016-5-30',
+  review: '<p>In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a orci. In aliquet facilisis condimentum. Donec at orci orci, a dictum justo. Sed a nunc non lectus fringilla suscipit. Vivamus pretium sapien sit amet mauris aliquet eleifend vel vitae arcu. Fusce pharetra dignissim nisl egestas pretium.</p><p>Proin ornare ligula eu tellus tempus elementum. Aenean bibendum iaculis mi, nec blandit lacus interdum vitae. Vestibulum non nibh risus, a scelerisque purus. Ut vel arcu ac tortor adipiscing hendrerit vel sed massa. Fusce sem libero, lacinia vulputate interdum non, porttitor non quam. Aliquam sed felis ligula. Duis non nulla magna.</p><p>In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a orci. In aliquet facilisis condimentum. Donec at orci orci, a dictum justo. Sed a nunc non lectus fringilla suscipit. Vivamus pretium sapien sit amet mauris aliquet eleifend vel vitae arcu. Fusce pharetra dignissim nisl egestas pretium.</p><p>Proin ut quam eros. Donec sed lobortis diam. Nulla nec odio lacus. Quisque porttitor egestas dolor in placerat. Nunc vehicula dapibus ipsum. Duis venenatis risus non nunc fermentum dapibus. Morbi lorem ante, malesuada in mollis nec, auctor nec massa. Aenean tempus dui eget felis blandit at fringilla urna ultrices. Suspendisse feugiat, ante et viverra lacinia, lectus sem lobortis dui, ultricies consectetur leo mauris at tortor. Nunc et tortor sit amet orci consequat semper. Nulla non fringilla diam.</p>'
+},
+{article: 'Monaco',
+  articleURL:'https://www.lonelyplanet.com/monaco',
+  category: 'Travel',
+  blogDate: '2016-5-30',
+  review: '<p>In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a orci. In aliquet facilisis condimentum. Donec at orci orci, a dictum justo. Sed a nunc non lectus fringilla suscipit. Vivamus pretium sapien sit amet mauris aliquet eleifend vel vitae arcu. Fusce pharetra dignissim nisl egestas pretium.</p><p>Proin ornare ligula eu tellus tempus elementum. Aenean bibendum iaculis mi, nec blandit lacus interdum vitae. Vestibulum non nibh risus, a scelerisque purus. Ut vel arcu ac tortor adipiscing hendrerit vel sed massa. Fusce sem libero, lacinia vulputate interdum non, porttitor non quam. Aliquam sed felis ligula. Duis non nulla magna.</p><p>In facilisis scelerisque dui vel dignissim. Sed nunc orci, ultricies congue vehicula quis, facilisis a orci. In aliquet facilisis condimentum. Donec at orci orci, a dictum justo. Sed a nunc non lectus fringilla suscipit. Vivamus pretium sapien sit amet mauris aliquet eleifend vel vitae arcu. Fusce pharetra dignissim nisl egestas pretium.</p><p>Proin ut quam eros. Donec sed lobortis diam. Nulla nec odio lacus. Quisque porttitor egestas dolor in placerat. Nunc vehicula dapibus ipsum. Duis venenatis risus non nunc fermentum dapibus. Morbi lorem ante, malesuada in mollis nec, auctor nec massa. Aenean tempus dui eget felis blandit at fringilla urna ultrices. Suspendisse feugiat, ante et viverra lacinia, lectus sem lobortis dui, ultricies consectetur leo mauris at tortor. Nunc et tortor sit amet orci consequat semper. Nulla non fringilla diam.</p>'
 }
 ];
--- javascript/display-data.js
+++ javascript/display-data.js
@@ -1,46 +1,127 @@
 //TODO: write the display functions to render the page
-var collection = [];
-
+var blogCollection = [];
+var categoryCollection = [];
 var Data = function(param){
-  this.library = param.library;
-  this.libraryURL = param.libraryURL;
+  this.article = param.article;
+  this.articleURL = param.articleURL;
   this.category = param.category;
-  this.dateTested = param.dateTested;
+  this.blogDate = param.blogDate;
   this.review = param.review;
 }
 
-Data.prototype.display = function(){
-  var $template = $('article.template').clone();
-  $template.attr('data-category',this.category).css('color','white');
-  $template.find('h1').css('margin','0px');
-  $template.find('h1>a').text(this.library).css({'text-decoration':'none','color':'white'});
-  $template.find('h1>a').attr('href',this.libraryURL);
-  $template.find('time').append(this.dateTested);
-  $template.find('#review-content').html(this.review);
-  $template.append('<hr>')
-  $template.removeClass('template');
-  return $template;
+var createArticle = function(element){
+  var $Article = jQuery('<article/>',{
+    'data-category':element.category,
+  });
+  var $URLHeader = jQuery('<h1/>').css('margin','0px');
+  var $URL = jQuery('<a/>',{
+    text:element.article,
+    href:element.articleURL
+  })
+  .css({'text-decoration':'none',
+    'color':'white'
+  });
+  var $Time = jQuery('<time/>',{
+    text:'Blog Written: '+element.blogDate
+  }).css('color','white');
+  var $ReviewContent = jQuery('<section/>',{
+    html:element.review
+  });
+  var $TeaserLink = jQuery('<a/>',{
+    text:"Read More...",
+    href:"#"
+  }).css({'text-decoration':'none',
+    'color':'white'
+  }).addClass('read-more');
+  var $HorizontalRule = jQuery('<hr/>').css('color','red');
+  $ReviewContent.find('p').css({'margin':'0px','color':'white','text-align':'justify'});
+  $URLHeader.append($URL);
+  $Article.append($URLHeader);
+  $Article.append($Time);
+  $Article.append($ReviewContent);
+  var numberOfParagraphs = $ReviewContent.find('p').length;
+  console.log(numberOfParagraphs);
+  if(numberOfParagraphs > 1){
+    $ReviewContent.find('p:nth-child(2n)').hide();
+    $Article.append($TeaserLink);
+  }
+  $Article.append($HorizontalRule);
+  $('#content-display').append($Article);
 }
 
-$(document).ready(function(){
-  searchResult.forEach(function(elem){//push all the data into the collection array
-    collection.push(new Data(elem))
-  });
+var populateCategoryDropdown = function(element){
+  var $categoryOption = jQuery('<option/>',{
+      text:element,
+      value:element
+    });
+  $('#category-filter').append($categoryOption);
+}
 
-  collection.forEach(function(elem){
-    $('#content-display').append(elem.display());
-  });
-  $('a.ion-document-text').on('click',function(e){
+var eventListeners = function(){
+    $('a.ion-document-text').on('click',function(e){
     e.preventDefault();
-    $('#about-me').hide();
-    $('#content-display').fadeIn(1000);
+    $('#about-me').hide()
+    $('#content-display').slideDown(500);
+
   })
   $('a.ion-ios-person').on('click',function(e){
     e.preventDefault();
     $('#content-display').hide();
-    $('#about-me').fadeIn(1000);
+    $('#about-me').slideDown(500)
   });
+  $('#category-filter').on('change',function(e){
+
+    var categoryValue = $(this).val();
+    if(categoryValue == ''){
+      $('#content-display > article').show();
+    }
+    var $Article = $('#content-display > article')
+    $Article.hide();
+    $("article[data-category='"+categoryValue+"'").slideDown(500);
+    $('#category-filter > option:eq(0)').text('--Show All--');
+  })
+  $('a.read-more').on('click',function(e){
+    e.preventDefault();
+    $(this).prev().find('p').slideDown(500);
+    $(this).hide();
+  });
+  $('.mobile-icon').on('click',function(e){
+    e.preventDefault();
+    $('.navigation-wrapper').slideDown(500);
+  });
+}
+
+var populatePageData = function(){
+    searchResult.forEach(function(elem){//push all the data into the collection array
+    blogCollection.push(new Data(elem))
+  });
+
+  blogCollection.forEach(function(elem){
+    createArticle(elem);
+  });
+  populateBlogFilter();
+  categoryCollection.forEach(function(elem){
+    populateCategoryDropdown(elem);
+  });
+}
+var populateBlogFilter = function(){
+  searchResult.forEach(function(elem){
+    var isInArray = $.inArray(elem.category,categoryCollection);
+    if(isInArray == -1){//-1 means it is not in the array
+      categoryCollection.push(elem.category);
+    }
+    else{
+      //do nothing
+    }
+  });
+}
+
+$(document).ready(function(){
+  populatePageData();
+  eventListeners();
   $('a.ion-document-text').click();
 });
+
+
 
 
