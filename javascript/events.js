03af2edc572e0973bb99b1c3b2b2be88e590237a
James Wortley <jameswortley@James-Wortleys-MacBook-Pro.local>
2016-06-19 15:33:07 -0700

    fixed nav issue with showAll

--- javascript/events.js
+++ javascript/events.js
@@ -1,26 +1,4 @@
-var showHome = function(){
-  $('a.ion-home').on('click',function(e){
-    e.preventDefault();
-    $('section').hide();
-    $('#home-wrapper').slideDown(400);
-  });
-}
 
-var showBlog = function(){
-    $('a.ion-document-text').on('click',function(e){
-    e.preventDefault();
-    $('section').hide()
-    $('#blog-display').slideDown(500);
-    enableTeaserLinks();
-  });
-}
-var showAboutMe = function(){
-    $('a.ion-ios-person').on('click',function(e){
-    e.preventDefault();
-    $('section').hide();
-    $('#about-me').slideDown(500)
-  });
-}
 
 var filterCategoryChange = function(){
   $('#category-filter').on('change',function(e){
@@ -83,4 +61,25 @@ $('a.ion-ios-person').on('click',function(e){
     $('section').hide();
     $('#about-me').slideDown(500)
 });
+
+//category filter change
+$('#category-filter').on('change',function(e){
+    var categoryValue = $(this).val();
+    var $Article = $('#blog-display > article');
+    $Article.slideUp(400);
+    if(categoryValue == ''){
+      $Article.slideDown(400);
+      $('#category-filter > option:eq(0)').text('--Select a Category--');
+    }
+    else{
+      $("article[data-category='"+categoryValue+"'").slideDown(400);
+      $('#category-filter > option:eq(0)').text('--Show All--');
+    }
+});
+
+//Mobile menu collapse
+$('.mobile-icon').on('click',function(e){
+    e.preventDefault();
+    $('.navigation-wrapper').toggle(500);
+  });
 
